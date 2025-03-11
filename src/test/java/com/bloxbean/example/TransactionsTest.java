bded1315baf7e0d28a8a7b446e4c6e4e3f503645
Satya <satran004@gmail.com>
2025-03-11 09:00:05 +0800

    Initial commit

--- src/test/java/com/bloxbean/example/TransactionsTest.java
+++ src/test/java/com/bloxbean/example/TransactionsTest.java
@@ -1,4 +1,169 @@
 package com.bloxbean.example;
 
+import com.bloxbean.cardano.client.account.Account;
+import com.bloxbean.cardano.client.api.model.Amount;
+import com.bloxbean.cardano.client.api.model.Result;
+import com.bloxbean.cardano.client.api.model.Utxo;
+import com.bloxbean.cardano.client.backend.api.BackendService;
+import com.bloxbean.cardano.client.backend.api.DefaultUtxoSupplier;
+import com.bloxbean.cardano.client.backend.blockfrost.service.BFBackendService;
+import com.bloxbean.cardano.client.backend.model.TransactionContent;
+import com.bloxbean.cardano.client.cip.cip20.MessageMetadata;
+import com.bloxbean.cardano.client.common.model.Networks;
+import com.bloxbean.cardano.client.function.helper.SignerProviders;
+import com.bloxbean.cardano.client.metadata.Metadata;
+import com.bloxbean.cardano.client.metadata.MetadataBuilder;
+import com.bloxbean.cardano.client.quicktx.QuickTxBuilder;
+import com.bloxbean.cardano.client.quicktx.Tx;
+import com.bloxbean.cardano.client.util.JsonUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TransactionsTest {
+    private static final String DEVKIT_INDEXER_BASE_URL = "http://localhost:8080/api/v1/";
+    private static final String DEVKIT_ADMIN_BASE_URL = "http://localhost:10000/";
+
+    private final static String MNEMONIC = "test test test test test test test test test test test test test test test test test test test test test test test sauce";
+    private final static Account sender = new Account(Networks.testnet(), MNEMONIC);
+
+    private String receiverAddress = "addr_test1qq8phk43ndg0zf2l4xc5vd7gu4f85swkm3dy7fjmfkf6q249ygmm3ascevccsq5l5ym6khc3je5plx9t5vsa06jvlzls8el07z";
+
+    private final static BackendService backendService = new BFBackendService(DEVKIT_INDEXER_BASE_URL,"dummy key");
+
+    @BeforeEach
+    void beforeEach() {
+        //If want to reset before every test
+//        resetDevNet();
+    }
+
+    @Test
+    void simplePayment() {
+        Metadata metadata = MetadataBuilder.createMetadata();
+        metadata.put(BigInteger.valueOf(100), "This is second metadata");
+        metadata.putNegative(200, -900);
+
+        QuickTxBuilder quickTxBuilder = new QuickTxBuilder(backendService);
+        Tx tx = new Tx()
+                .payToAddress(receiverAddress, Amount.ada(1.5))
+                .attachMetadata(MessageMetadata.create().add("This is a test message"))
+                .from(sender.baseAddress());
+
+        Result<String> result = quickTxBuilder.compose(tx)
+                .withSigner(SignerProviders.signerFrom(sender))
+                .complete();
+
+        System.out.println(result);
+
+        System.out.println(result);
+        assertThat(result.isSuccessful());
+
+        waitForTransaction(result);
+    }
+
+    public void waitForTransaction(Result<String> result) {
+        try {
+            if (result.isSuccessful()) { //Wait for transaction to be mined
+                int count = 0;
+                while (count < 60) {
+                    Result<TransactionContent> txnResult = backendService.getTransactionService().getTransaction(result.getValue());
+                    if (txnResult.isSuccessful()) {
+                        System.out.println(JsonUtil.getPrettyJson(txnResult.getValue()));
+                        break;
+                    } else {
+                        System.out.println("Waiting for transaction to be mined ....");
+                    }
+
+                    count++;
+                    Thread.sleep(2000);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void checkIfUtxoAvailable(String txHash, String address) {
+        Optional<Utxo> utxo = Optional.empty();
+        int count = 0;
+        while (utxo.isEmpty()) {
+            if (count++ >= 20)
+                break;
+            List<Utxo> utxos = new DefaultUtxoSupplier(backendService.getUtxoService()).getAll(address);
+            utxo = utxos.stream().filter(u -> u.getTxHash().equals(txHash))
+                    .findFirst();
+            System.out.println("Try to get new output... txhash: " + txHash);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {}
+        }
+    }
+
+    protected static void topUpFund(String address, long adaAmount) {
+        try {
+            // URL to the top-up API
+            String url = DEVKIT_ADMIN_BASE_URL + "local-cluster/api/addresses/topup";
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            // Set request method to POST
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+
+            // Create JSON payload
+            String jsonInputString = String.format("{\"address\": \"%s\", \"adaAmount\": %d}", address, adaAmount);
+
+            // Send the request
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            // Check the response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Funds topped up successfully.");
+            } else {
+                System.out.println("Failed to top up funds. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static void resetDevNet() {
+        try {
+            // URL to reset the network
+            String url = DEVKIT_ADMIN_BASE_URL + "local-cluster/api/admin/devnet/reset";
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            // Set request method to POST
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoOutput(true);
+
+            // Check the response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Devnet reset successful");
+            } else {
+                System.out.println("Failed to reset the network. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
